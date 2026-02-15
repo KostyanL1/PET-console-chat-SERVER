5cf342ca0b3f6276c6058a4266eadf1840ea30d0
Костя Легенький <kostyantim.l05@gmail.com>
2026-02-15 18:21:37 +0200

    Created method register with hased password.

--- src/main/java/org/legenkiy/api/service/AuthService.java
+++ src/main/java/org/legenkiy/api/service/AuthService.java
@@ -1,10 +1,14 @@
 package org.legenkiy.api.service;
 
+import org.legenkiy.protocol.dtos.AuthDto;
+
 import java.net.Socket;
 
 public interface AuthService {
 
+    void register(Socket socket, AuthDto authDto);
     void login(Socket socket, String username);
     boolean isAuthenticate(Socket socket);
+    boolean isRegistered(String username);
 
 }
--- src/main/java/org/legenkiy/services/AuthServiceImpl.java
+++ src/main/java/org/legenkiy/services/AuthServiceImpl.java
@@ -4,10 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.legenkiy.api.service.AuthService;
+import org.legenkiy.api.service.UserService;
 import org.legenkiy.connection.ConnectionsManager;
+import org.legenkiy.dto.UserDto;
 import org.legenkiy.enums.ClientState;
 import org.legenkiy.exceptions.AuthException;
+import org.legenkiy.exceptions.ObjectNotFoundException;
 import org.legenkiy.models.ActiveConnection;
+import org.legenkiy.protocol.dtos.AuthDto;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 import java.net.Socket;
@@ -20,6 +25,29 @@ public class AuthServiceImpl implements AuthService {
     private final static Logger LOGGER = LogManager.getLogger(AuthServiceImpl.class);
 
     private final ConnectionsManager connectionsManager;
+    private final UserService userService;
+
+
+    @Override
+    public void register(Socket socket, AuthDto authDto){
+        if (!isAuthenticate(socket)){
+            if (!isRegistered(authDto.getUsername())){
+                UserDto userDto = new UserDto();
+                userDto.setUsername(userDto.getUsername());
+                userDto.setPassword(BCrypt.hashpw(authDto.getPassword(), BCrypt.gensalt()));
+                userService.save(userDto);
+
+
+            }else {
+                LOGGER.info("This username already exist {}", authDto.getUsername());
+                throw new AuthException("This username already exist " + socket.getRemoteSocketAddress());
+            }
+        }else {
+            LOGGER.info("This socket authenticated {}", socket.getRemoteSocketAddress());
+            throw new AuthException("This socket authenticated " + socket.getRemoteSocketAddress());
+        }
+
+    }
 
 
     @Override
@@ -27,7 +55,7 @@ public class AuthServiceImpl implements AuthService {
         if (!isAuthenticate(socket)){
             connectionsManager.authenticate(socket, username);
         }else {
-            LOGGER.info("This socket authenticated {}", socket.getRemoteSocketAddress());
+
             throw new AuthException("This socket authenticated " + socket.getRemoteSocketAddress());
         }
     }
@@ -38,5 +66,15 @@ public class AuthServiceImpl implements AuthService {
     public boolean isAuthenticate(Socket socket) {
         Optional<ActiveConnection> activeConnection = Optional.ofNullable(connectionsManager.findConnectionBySocket(socket));
         return activeConnection.map(connection -> connection.getClientState().equals(ClientState.AUTHENTICATED)).orElse(false);
+    }
+
+    @Override
+    public boolean isRegistered(String username){
+        try {
+            userService.findByUsername(username);
+            return true;
+        }catch (ObjectNotFoundException e){
+            return false;
+        }
     }
 }
